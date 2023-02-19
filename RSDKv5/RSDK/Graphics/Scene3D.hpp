fba02ef1b919a39ad1d190ac47c9c1ce7c8db9bf
Saturn <helpithinkihavehermes@gmail.com>
2023-02-19 07:27:05 -0500

    comment that out for a performance boost

--- RSDKv5/RSDK/Graphics/Scene3D.hpp
+++ RSDKv5/RSDK/Graphics/Scene3D.hpp
@@ -154,10 +154,11 @@ inline void Prepare3DScene(uint16 sceneID)
         scn->vertexCount = 0;
         scn->faceCount   = 0;
 
-        memset(scn->vertices, 0, sizeof(Scene3DVertex) * scn->vertLimit);
-        memset(scn->normals, 0, sizeof(Scene3DVertex) * scn->vertLimit);
-        memset(scn->faceVertCounts, 0, sizeof(uint8) * scn->vertLimit);
-        memset(scn->faceBuffer, 0, sizeof(Scene3DFace) * scn->vertLimit);
+        // commenting out for performance gain, thanks @Mephiles
+        //memset(scn->vertices, 0, sizeof(Scene3DVertex) * scn->vertLimit);
+        //memset(scn->normals, 0, sizeof(Scene3DVertex) * scn->vertLimit);
+        //memset(scn->faceVertCounts, 0, sizeof(uint8) * scn->vertLimit);
+        //memset(scn->faceBuffer, 0, sizeof(Scene3DFace) * scn->vertLimit);
     }
 }
 
