228191e6c781db4af32c0e066e788c468de9113e
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2023-05-05 18:09:16 +0200

    workflow: Update Github Action + CIA build script

--- 3ds/build_cia.sh
+++ 3ds/build_cia.sh
@@ -1,21 +1,24 @@
-#!/bin/bash 
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Missing path to elf file."
+    echo "Usage: $0 path/to/file.elf"
+    exit 1
+fi
 
 APP_NAME="Sonic Mania"
 APP_AUTH="SaturnSH2x2, Rubberduckycooly"
 
-mkdir -p ../bin/3DS/CTR/out &&
-rm -rf ../bin/3DS/CTR/out/*
-rm -rf ../bin/3DS/CTR/SonicMania.cia
+mkdir -p out
+rm -rf out/*
 
 # make banner
-./bannertool makebanner -i ./banner.png -a ./jingle.wav -o ../bin/3DS/CTR/out/banner.bnr &&
+./bannertool makebanner -i ./banner.png -a ./jingle.wav -o out/banner.bnr
 
 # make SMDH
-./bannertool makesmdh -s $APP_NAME -l $APP_NAME -p $APP_AUTH -i ./48x48.png -o ../bin/3DS/CTR/out/icon.icn &&
+./bannertool makesmdh -s "$APP_NAME" -l "$APP_NAME" -p "$APP_AUTH" -i ./48x48.png -o out/icon.icn
 
 # build CIA
-./makerom -f cia -o ../bin/3DS/CTR/SonicMania.cia -DAPP_ENCRYPTED=false -elf ../bin/3DS/CTR/RSDKv5.elf -rsf ./SonicMania.rsf -exefslogo -target t -icon ../bin/3DS/CTR/out/icon.icn -banner ../bin/3DS/CTR/out/banner.bnr &&
+./makerom -f cia -o out/SonicMania.cia -DAPP_ENCRYPTED=false -elf "$1" -rsf ./SonicMania.rsf -exefslogo -target t -icon out/icon.icn -banner out/banner.bnr
 
 echo "Built .cia"
-
-rm -rf ../bin/3DS/CTR/out
