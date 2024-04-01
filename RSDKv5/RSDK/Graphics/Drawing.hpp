4b395130db5d5ccfeb549157b4970ceb2656ca40
smb123w64gb <smb123w64gb@gmail.com>
2024-04-01 05:57:43 -0700

    Update Drawing.hpp

--- RSDKv5/RSDK/Graphics/Drawing.hpp
+++ RSDKv5/RSDK/Graphics/Drawing.hpp
@@ -23,7 +23,7 @@ namespace RSDK
 #define SHADER_COUNT (0x20)
 
 // Also for "Images" but it's a cleaner name as is
-#define RETRO_VIDEO_TEXTURE_W (1024)
+#define RETRO_VIDEO_TEXTURE_W (512)
 #define RETRO_VIDEO_TEXTURE_H (512)
 
 enum InkEffects {
